d15312945363324620cae2cbfc49cdb687a944c4
ilovezy <1518265554@qq.com>
2018-10-28 12:39:03 +0800

    change qrcode

--- src/global/CONFIG.js
+++ src/global/CONFIG.js
@@ -3,7 +3,7 @@ window.CONFIG = {
     'localhost': '//10.17.13.231:10010/',   //本地开发构建环境 npm run buildlocal
     'uat': '//10.17.13.231:8088/',   //uat构建环境 npm run builduat
     'test': '//10.17.13.231:10010/',   //测试构建环境 npm run buildtest
-    'production': '//api.changfulicai.com/',  //上线构建环境  npm run buildproduction
+    'production': '//118.25.23.224:9001/',  //上线构建环境  npm run buildproduction
 
     //热更新开发构建环境 npm run dev
     // 'development': '//10.60.10.165:9001/', // dev环境
